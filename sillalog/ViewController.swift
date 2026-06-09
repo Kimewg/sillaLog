cb4290ac79ac6d339ef987f0fa878b2f51f9b60b
eunseo <sawol2239@gmail.com>
2026-06-09 16:30:28 +0900

    Initial Commit

---
+++ sillalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sillalog
+//
+//  Created by 김은서 on 6/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
